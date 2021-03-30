43173dd7d64f9515e2602c9e819c2c539f1df867
tedsmoore <tedsmoore@gmail.com>
2021-03-30 14:46:23 -0400

    a form view

--- definitive/urls.py
+++ definitive/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('ranks/<int:list_id>', views.rankview, name='index'),
+    path('ranks/<int:list_id>/add-item', views.new_item, name='new-item')
 ]
 
 # if settings.DEBUG:
--- definitive/views.py
+++ definitive/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 from definitive.models import RankList
+from definitive.forms import AddRankItemForm
 
 
 def rankview(request, list_id):
@@ -10,3 +11,15 @@ def rankview(request, list_id):
         'items': items
     }
     return render(request, 'ranklist.html', context)
+
+
+def new_item(request, list_id):
+    rank_list =RankList.objects.get(id=list_id)
+    form = AddRankItemForm
+    context = {
+        'form': form,
+        'rank_list': rank_list
+    }
+    return render(request, 'new_item.html', context)
+
+
