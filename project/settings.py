e89b7a15432361d29205c4cc3bc58ed3ee7c0519
tedsmoore <tedsmoore@gmail.com>
2021-05-08 12:43:46 -0400

    logging config

--- project/settings.py
+++ project/settings.py
@@ -147,3 +147,17 @@ STATICFILES_DIRS = [
 # User uploaded files
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = 'media/'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'INFO',
+    },
+}
